0757d9269392740d5bbae8fa9fe43cc27022094c
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-11-14 16:15:16 -0300

    Create 05MysqlOnShell.sql

---
+++ Curso/05MysqlOnShell.sql
@@ -0,0 +1,9 @@
+/*
+Como abrir o MySQL no XAMPP pelo prompt de comando:
+
+1- abre o XAMPP
+2- clica em Shell
+3- cd c:/xampp/mysql/bin (ENTER)
+4- mysql -u root -h 127.0.0.1 -p (ENTER)
+5- Password (ENTER)
+*/
